1995d65a4f2ce8b90e5c4c28b42c8b034aeb12a9
Kristina Lubinets <kris.lubinec@gmail.com>
2024-01-01 19:38:56 -0500

    added readme'

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ from pydantic import BaseModel
 # Replace these with your actual imports from your SQLAlchemy setup
 # from database import Base, engine, get_db
 
+# defines a connection URL for a PostgreSQL database
 SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypassword@database/mydatabase"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
